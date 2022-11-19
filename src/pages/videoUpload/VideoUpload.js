65bac48b706206cfc87a9a336355ea08b2d09ae1
Muskan Kapoor <muskankapoor927@gmail.com>
2022-11-19 08:22:10 -0800

    Added the handleclick function to the form in the upload page so that when a user submits a new video, it posts the video to the API in order to save it to the list of videos.Deleted the publish page

--- src/pages/videoUpload/VideoUpload.js
+++ src/pages/videoUpload/VideoUpload.js
@@ -1,11 +1,29 @@
 import "./VideoUpload.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function VideoUpload (){
+    const navigate=useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newVideo = {
+          title:event.target.title.value,
+          description:event.target.description.value,
+        };
+        console.log(event.target.title.value);
+       axios
+          .post(`http://localhost:8080/videos`, newVideo)
+          .then((response) => {
+           console.log(response);
+          });
+          navigate("/")
+      };
     return (
     <section className="upload">
         <div className="upload__wrapper">
         <h1 className="upload__heading">Upload Video</h1>
+        <form onSubmit={handleSubmit}>
         <div className="upload__flex-wrapper">
         <div className="upload__thumbnail-wrapper">
         <h2 className="upload__subheading">VIDEO THUMBNAIL</h2>
@@ -13,19 +31,19 @@ export default function VideoUpload (){
         </div>
        <div className="upload__form-wrapper">
         <div className="upload__title-wrapper">
-        <h2 className="upload__subheading">TITLE YOUR VIDEO</h2>
-        <input type="text" name="text" placeholder="Add a title to your video" className="upload__input upload__input--title" /> 
+        <label htmlFor="text" className="upload__subheading">TITLE YOUR VIDEO</label>
+        <input type="text" id="text" name="title" placeholder="Add a title to your video" className="upload__input upload__input--title" /> 
         </div>
         <div className="upload__description-wrapper">
-        <h2 className="upload__subheading">ADD A VIDEO DESCRIPTION</h2>
-        <input type="textarea" name="textarea" placeholder="Add a description to your video" className="upload__input upload__input--description" /> 
+        <label htmlFor="description" className="upload__subheading">ADD A VIDEO DESCRIPTION</label>
+        <input type="text" id="description" name="description" placeholder="Add a description to your video" className="upload__input upload__input--description" /> 
         </div>
         </div>
         </div>
-        <div className="upload__button-wrapper">
-        <Link to={"/publish"}>
+        {/* <div className="upload__button-wrapper"> */}
+        
         <button type="submit" className="upload__button upload__button--publish">PUBLISH</button>
-        </Link>
+       </form>
         <Link to={"/"}>
         <button type="submit" className="upload__button upload__button--cancel">CANCEL</button>
         </Link>
@@ -33,7 +51,7 @@ export default function VideoUpload (){
         </div>
         
         
-        </div>
+        {/* </div> */}
         </section>
     );
 }
